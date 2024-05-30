bd42636287460f6ce0595d0303cde638e8ae8ce5
Sébastien Weber <seba.weber@gmail.com>
2024-05-30 14:52:52 +0200

    make sure tests are running

--- src/pymodaq_plugins_teaching/__init__.py
+++ src/pymodaq_plugins_teaching/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
+from .utils import Config
+config = Config()
+
 with open(str(Path(__file__).parent.joinpath('resources/VERSION')), 'r') as fvers:
     __version__ = fvers.read().strip()
